454cc6823495ec133ccc9f555fa92252b74210d3
avi <avi.vajpeyi@gmail.com>
2024-12-06 14:53:32 +1300

    add template

---
+++ study/likelihood_comparison.py
@@ -0,0 +1,12 @@
+"""
+Here we compare the Likelihood of the original and the new MultivariatePSD likelihood for the same data, 
+
+"""
+
+
+# from basic import Likelihood
+# from new import MultivarPSDLikelihood
+# parameter = [1,0,0,0]
+# data = loda_data()
+# assert Likelihood(data, parameter) == MultiavarPSDLikelihood(data, parameter)   
+
